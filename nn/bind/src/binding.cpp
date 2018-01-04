8cf6374c11654db98b8f7819199f04ecb0ca4a29
Ningxin Hu <ningxin.hu@intel.com>
2018-01-04 13:34:24 +0800

    Add conv and depthwiseConv

--- nn/bind/src/binding.cpp
+++ nn/bind/src/binding.cpp
@@ -27,8 +27,129 @@ namespace binding_utils {
     shape.dimensions = vecFromJSArray<uint32_t>(js_dims);
   }
 
-  bool addFloat32Wrapper(intptr_t in1, const Shape& shape1, intptr_t in2, const Shape& shape2, int32_t activation, intptr_t out, const Shape& shapeOut) {
+  // Operation helper wrappers.
+  bool addMulPrepareWrapper(const Shape& in1, const Shape& in2, Shape& out1) {
+    return addMulPrepare(in1, in2, &out1);
+  }
+
+  bool floorPrepareWrapper(const Shape& input, Shape& output) {
+    return floorPrepare(input, &output);
+  }
+
+  bool dequantizePrepareWrapper(const Shape& input, Shape& output) {
+    return dequantizePrepare(input, &output);
+  }
+
+  bool depthwiseConvPrepareWrapper(const Shape& input, const Shape& filter, const Shape& bias,
+                                   int32_t padding_left, int32_t padding_right, int32_t padding_top, int32_t padding_bottom,
+                                   int32_t stride_width, int32_t stride_height, Shape& output) {
+    return depthwiseConvPrepare(input, filter, bias, padding_left, padding_right, padding_top, padding_bottom, stride_width, stride_height, &output);
+  }
+
+  bool convPrepareWrapper(const Shape& input, const Shape& filter, const Shape& bias,
+                          int32_t padding_left, int32_t padding_right, int32_t padding_top, int32_t padding_bottom,
+                          int32_t stride_width, int32_t stride_height, Shape& output) {
+    return convPrepare(input, filter, bias, padding_left, padding_right, padding_top, padding_bottom, stride_width, stride_height, &output);
+  }
+
+  // Operation wrappers.
+  bool addFloat32Wrapper(const intptr_t in1, const Shape& shape1,
+                         const intptr_t in2, const Shape& shape2,
+                         int32_t activation, intptr_t out, const Shape& shapeOut) {
     return addFloat32((const float*)in1, shape1, (const float*)in2, shape2, activation, (float*)out, shapeOut);
+  }
+
+  bool addQuant8Wrapper(const intptr_t in1, const Shape& shape1,
+                        const intptr_t in2, const Shape& shape2,
+                        int32_t activation, intptr_t out, const Shape& shapeOut) {
+    return addQuant8((const uint8_t*)in1, shape1, (const uint8_t*)in2, shape2, activation, (uint8_t*)out, shapeOut);
+  }
+
+  bool mulFloat32Wrapper(const intptr_t in1, const Shape& shape1,
+                         const intptr_t in2, const Shape& shape2,
+                         int32_t activation, intptr_t out, const Shape& shapeOut) {
+    return mulFloat32((const float*)in1, shape1, (const float*)in2, shape2, activation, (float*)out, shapeOut);
+  }
+
+  bool mulQuant8Wrapper(const intptr_t in1, const Shape& shape1,
+                        const intptr_t in2, const Shape& shape2,
+                        int32_t activation, intptr_t out, const Shape& shapeOut) {
+    return mulQuant8((const uint8_t*)in1, shape1, (const uint8_t*)in2, shape2, activation, (uint8_t*)out, shapeOut);
+  }
+
+  bool floorFloat32Wrapper(const intptr_t inputData, intptr_t outputData, const Shape& shape) {
+    return floorFloat32((const float*)inputData, (float*)outputData, shape);
+  }
+
+  bool dequantizeQuant8ToFloat32Wrapper(const intptr_t inputData, intptr_t outputData, const Shape& shape) {
+    return dequantizeQuant8ToFloat32((const uint8_t*)inputData, (float*)outputData, shape);
+  }
+
+  bool depthwiseConvFloat32Wrapper(const intptr_t inputData, const Shape& inputShape,
+                                   const intptr_t filterData, const Shape& filterShape,
+                                   const intptr_t biasData, const Shape& biasShape,
+                                   int32_t padding_left, int32_t padding_right,
+                                   int32_t padding_top, int32_t padding_bottom,
+                                   int32_t stride_width, int32_t stride_height,
+                                   int32_t depth_multiplier, int32_t activation,
+                                   intptr_t outputData, const Shape& outputShape) {
+    return depthwiseConvFloat32((const float*)inputData, inputShape,
+                                (const float*)filterData, filterShape,
+                                (const float*)biasData, biasShape,
+                                padding_left, padding_right, padding_top, padding_bottom,
+                                stride_width, stride_height,
+                                depth_multiplier, activation,
+                                (float*)outputData, outputShape);
+  }
+
+  bool depthwiseConvQuant8Wrapper(const intptr_t inputData, const Shape& inputShape,
+                                  const intptr_t filterData, const Shape& filterShape,
+                                  const intptr_t biasData, const Shape& biasShape,
+                                  int32_t padding_left, int32_t padding_right,
+                                  int32_t padding_top, int32_t padding_bottom,
+                                  int32_t stride_width, int32_t stride_height,
+                                  int32_t depth_multiplier, int32_t activation,
+                                  intptr_t outputData, const Shape& outputShape) {
+    return depthwiseConvQuant8((const uint8_t*)inputData, inputShape,
+                               (const uint8_t*)filterData, filterShape,
+                               (const int32_t*)biasData, biasShape,
+                               padding_left, padding_right,
+                               padding_top, padding_bottom,
+                               stride_width, stride_height,
+                               depth_multiplier, activation,
+                               (uint8_t*)outputData, outputShape);
+  }
+
+  bool convFloat32Wrapper(const intptr_t inputData, const Shape& inputShape,
+                          const intptr_t filterData, const Shape& filterShape,
+                          const intptr_t biasData, const Shape& biasShape,
+                          int32_t padding_left, int32_t padding_right,
+                          int32_t padding_top, int32_t padding_bottom,
+                          int32_t stride_width, int32_t stride_height,
+                          int32_t activation,
+                          intptr_t outputData, const Shape& outputShape) {
+    return convFloat32((const float*)inputData, inputShape,
+                       (const float*)filterData, filterShape,
+                       (const float*)biasData, biasShape,
+                       padding_left, padding_right, padding_top, padding_bottom,
+                       stride_width,  stride_height, activation,
+                       (float*)outputData, outputShape);
+  }
+  bool convQuant8Wrapper(const intptr_t inputData, const Shape& inputShape,
+                         const intptr_t filterData, const Shape& filterShape,
+                         const intptr_t biasData, const Shape& biasShape,
+                         int32_t padding_left, int32_t padding_right,
+                         int32_t padding_top, int32_t padding_bottom,
+                         int32_t stride_width, int32_t stride_height,
+                         int32_t activation,
+                         intptr_t outputData, const Shape& outputShape) {
+    return convQuant8((const uint8_t*)inputData, inputShape,
+                      (const uint8_t*)filterData, filterShape,
+                      (const int32_t*)biasData, biasShape,
+                      padding_left, padding_right,
+                      padding_top, padding_bottom,
+                      stride_width, stride_height, activation,
+                      (uint8_t*)outputData, outputShape);
   }
 }
 
@@ -54,16 +175,24 @@ EMSCRIPTEN_BINDINGS(nn)
     .property("offset", &Shape::offset)
     ;
 
+  // Operation helpers.
+  function("addMulPrepare", &binding_utils::addMulPrepareWrapper);
+  function("floorPrepare", &binding_utils::floorPrepareWrapper);
+  function("dequantizePrepare", &binding_utils::dequantizePrepareWrapper);
+  function("depthwiseConvPrepare", &binding_utils::depthwiseConvPrepareWrapper);
+  function("convPrepare", &binding_utils::convPrepareWrapper);
+
+  // Operations.
   function("addFloat32", &binding_utils::addFloat32Wrapper, allow_raw_pointers());
-  function("addQuant8", &addQuant8, allow_raw_pointers());
-  function("mulFloat32", &mulFloat32, allow_raw_pointers());
-  function("mulQuant8", &mulQuant8, allow_raw_pointers());
-  function("floorFloat32", &floorFloat32, allow_raw_pointers());
-  function("dequantizeQuant8ToFloat32", &dequantizeQuant8ToFloat32, allow_raw_pointers());
-  function("depthwiseConvFloat32", &depthwiseConvFloat32, allow_raw_pointers());
-  function("depthwiseConvQuant8", &depthwiseConvQuant8, allow_raw_pointers());
-  function("convFloat32", &convFloat32, allow_raw_pointers());
-  function("convQuant8", &convQuant8, allow_raw_pointers());
+  function("addQuant8", &binding_utils::addQuant8Wrapper, allow_raw_pointers());
+  function("mulFloat32", &binding_utils::mulFloat32Wrapper, allow_raw_pointers());
+  function("mulQuant8", &binding_utils::mulQuant8Wrapper, allow_raw_pointers());
+  function("floorFloat32", &binding_utils::floorFloat32Wrapper, allow_raw_pointers());
+  function("dequantizeQuant8ToFloat32", &binding_utils::dequantizeQuant8ToFloat32Wrapper, allow_raw_pointers());
+  function("depthwiseConvFloat32", &binding_utils::depthwiseConvFloat32Wrapper, allow_raw_pointers());
+  function("depthwiseConvQuant8", &binding_utils::depthwiseConvQuant8Wrapper, allow_raw_pointers());
+  function("convFloat32", &binding_utils::convFloat32Wrapper, allow_raw_pointers());
+  function("convQuant8", &binding_utils::convQuant8Wrapper, allow_raw_pointers());
   function("averagePoolFloat32", &averagePoolFloat32, allow_raw_pointers());
   function("averagePoolQuant8", &averagePoolQuant8, allow_raw_pointers());
   function("l2PoolFloat32", &l2PoolFloat32, allow_raw_pointers());
--- nn/bind/test/operations.js
+++ nn/bind/test/operations.js
@@ -4,43 +4,198 @@ describe('Operations Test', function() {
   function almostEqual(a, b) {
     const FLOAT_EPISILON = 1e-6;
     let delta = Math.abs(a - b);
-    return delta < FLOAT_EPISILON;
+    if (delta < FLOAT_EPISILON) {
+      return true;
+    } else {
+      console.warn(`a(${a}) b(${b}) delta(${delta})`);
+      return false;
+    }
   }
 
   it('addFloat32', function() {
-    let tensor1Data = new Float32Array([0.5, 0.5, 0.5, 0.5]);
-    let tensor1ByteLength = tensor1Data.length * tensor1Data.BYTES_PER_ELEMENT;
-    let tensor1 = Module._malloc(tensor1ByteLength);
-    let dataHeap = new Uint8Array(Module.HEAPU8.buffer, tensor1, tensor1ByteLength);
-    dataHeap.set( new Uint8Array(tensor1Data.buffer) );
+    const value1 = 0.5;
+    const value2 = 0.5;
+    const dims = [1, 227, 227, 3];
+  
     let shape1 = new Module.Shape;
     shape1.type = Module.TENSOR_FLOAT32;
-    shape1.dimensions = [5];
-
-    let tensor2Data = new Float32Array([0.1, 0.1, 0.1, 0.1]);
-    let tensor2ByteLength = tensor2Data.length * tensor2Data.BYTES_PER_ELEMENT;
-    let tensor2 = Module._malloc(tensor2ByteLength);
-    dataHeap = new Uint8Array(Module.HEAPU8.buffer, tensor2, tensor2ByteLength);
-    dataHeap.set( new Uint8Array(tensor2Data.buffer) );
+    shape1.dimensions = dims;
+    let tensor1Data = new Float32Array(product(shape1.dimensions));
+    tensor1Data.fill(value1);
+    let tensor1ByteLength = tensor1Data.length * tensor1Data.BYTES_PER_ELEMENT;
+    let tensor1 = Module._malloc(tensor1ByteLength);
+    let heap = new Uint8Array(Module.HEAPU8.buffer, tensor1, tensor1ByteLength);
+    heap.set( new Uint8Array(tensor1Data.buffer) );
+    
     let shape2 = new Module.Shape;
     shape2.type = Module.TENSOR_FLOAT32;
-    shape2.dimensions = [5];
+    shape2.dimensions = dims;
+    let tensor2Data = new Float32Array(product(shape2.dimensions));
+    tensor2Data.fill(value2);
+    let tensor2ByteLength = tensor2Data.length * tensor2Data.BYTES_PER_ELEMENT;
+    let tensor2 = Module._malloc(tensor2ByteLength);
+    heap = new Uint8Array(Module.HEAPU8.buffer, tensor2, tensor2ByteLength);
+    heap.set( new Uint8Array(tensor2Data.buffer) );
 
-    let tensor3Data = new Float32Array(4);
-    tensor3Data.fill(0);
-    let tensor3ByteLength = tensor3Data.length * tensor3Data.BYTES_PER_ELEMENT;
-    let tensor3 = Module._malloc(tensor2ByteLength);
     let shape3 = new Module.Shape;
     shape3.type = Module.TENSOR_FLOAT32;
-    shape3.dimensions = [5];
-    Module.addFloat32(tensor1, shape1, tensor2, shape2, Module.NONE, tensor3, shape3);
-    
-    dataHeap = new Uint8Array(Module.HEAPU8.buffer, tensor3, tensor3ByteLength);
-    tensor3Buffer = new Uint8Array(tensor3Data.buffer);
-    tensor3Buffer.set(dataHeap);
+    shape3.dimensions = [0, 0, 0, 0];
 
-    for (let i = 0; i < tensor1Data.length; ++i) {
+    assert.isTrue(Module.addMulPrepare(shape1, shape2, shape3));
+    assert.deepEqual(shape3.dimensions, dims);
+
+    let tensor3Data = new Float32Array(product(shape3.dimensions));
+    tensor3Data.fill(0);
+    let tensor3ByteLength = tensor3Data.length * tensor3Data.BYTES_PER_ELEMENT;
+    let tensor3 = Module._malloc(tensor3ByteLength);
+
+    let start = performance.now();
+    assert.isTrue(Module.addFloat32(tensor1, shape1, tensor2, shape2, Module.NONE, tensor3, shape3));
+    let end = performance.now();
+    console.log(`addFloat32 elapsed time: ${end - start} ms`);
+
+    heap = new Uint8Array(Module.HEAPU8.buffer, tensor3, tensor3ByteLength);
+    tensor3Buffer = new Uint8Array(tensor3Data.buffer);
+    tensor3Buffer.set(heap);
+
+    for (let i = 0; i < 10; ++i) {
       assert.isTrue(almostEqual(tensor3Data[i], tensor1Data[i] + tensor2Data[i]));
     }
+
+    Module._free(tensor1);
+    Module._free(tensor2);
+    Module._free(tensor3);
+    shape1.delete();
+    shape2.delete();
+    shape3.delete();
+  });
+
+  it('mulFloat32', function() {
+    const value1 = 0.5;
+    const value2 = 0.5;
+    const dims = [1, 227, 227, 3];
+  
+    let shape1 = new Module.Shape;
+    shape1.type = Module.TENSOR_FLOAT32;
+    shape1.dimensions = dims;
+    let tensor1Data = new Float32Array(product(shape1.dimensions));
+    tensor1Data.fill(value1);
+    let tensor1ByteLength = tensor1Data.length * tensor1Data.BYTES_PER_ELEMENT;
+    let tensor1 = Module._malloc(tensor1ByteLength);
+    let heap = new Uint8Array(Module.HEAPU8.buffer, tensor1, tensor1ByteLength);
+    heap.set( new Uint8Array(tensor1Data.buffer) );
+    
+    let shape2 = new Module.Shape;
+    shape2.type = Module.TENSOR_FLOAT32;
+    shape2.dimensions = dims;
+    let tensor2Data = new Float32Array(product(shape2.dimensions));
+    tensor2Data.fill(value2);
+    let tensor2ByteLength = tensor2Data.length * tensor2Data.BYTES_PER_ELEMENT;
+    let tensor2 = Module._malloc(tensor2ByteLength);
+    heap = new Uint8Array(Module.HEAPU8.buffer, tensor2, tensor2ByteLength);
+    heap.set( new Uint8Array(tensor2Data.buffer) );
+
+    let shape3 = new Module.Shape;
+    shape3.type = Module.TENSOR_FLOAT32;
+    shape3.dimensions = [0, 0, 0, 0];
+
+    assert.isTrue(Module.addMulPrepare(shape1, shape2, shape3));
+    assert.deepEqual(shape3.dimensions, dims);
+
+    let tensor3Data = new Float32Array(product(shape3.dimensions));
+    tensor3Data.fill(0);
+    let tensor3ByteLength = tensor3Data.length * tensor3Data.BYTES_PER_ELEMENT;
+    let tensor3 = Module._malloc(tensor3ByteLength);
+
+    let start = performance.now();
+    assert.isTrue(Module.mulFloat32(tensor1, shape1, tensor2, shape2, Module.NONE, tensor3, shape3));
+    let end = performance.now();
+    console.log(`mulFloat32 elapsed time: ${end - start} ms`);
+
+    heap = new Uint8Array(Module.HEAPU8.buffer, tensor3, tensor3ByteLength);
+    tensor3Buffer = new Uint8Array(tensor3Data.buffer);
+    tensor3Buffer.set(heap);
+
+    for (let i = 0; i < 10; ++i) {
+      assert.isTrue(almostEqual(tensor3Data[i], tensor1Data[i] * tensor2Data[i]));
+    }
+
+    Module._free(tensor1);
+    Module._free(tensor2);
+    Module._free(tensor3);
+    shape1.delete();
+    shape2.delete();
+    shape3.delete();
+  });
+
+  function randomFill(data) {
+    for (let i = 0; i < data.length; ++i) {
+      data[i] = Math.random() * 2 - 1
+    }
+  }
+
+  function product(array) {
+    return array.reduce((accumulator, currentValue) => accumulator * currentValue);
+  }
+
+  it('convFloat32', function() {
+    const padding = 0;
+    const stride = 4;
+    
+    let inputShape = new Module.Shape;
+    inputShape.type = Module.TENSOR_FLOAT32;
+    inputShape.dimensions = [1, 227, 227, 3];
+
+    let filterShape = new Module.Shape;
+    filterShape.type = Module.TENSOR_FLOAT32;
+    filterShape.dimensions = [96, 11, 11, 3];
+
+    let biasShape = new Module.Shape;
+    biasShape.type = Module.TENSOR_FLOAT32;
+    biasShape.dimensions = [96];
+
+    let outputShape = new Module.Shape;
+
+    assert.isTrue(Module.convPrepare(inputShape, filterShape, biasShape, padding, padding, padding, padding, stride, stride, outputShape));
+
+    assert.equal(outputShape.type, Module.TENSOR_FLOAT32);
+    assert.deepEqual(outputShape.dimensions, [1, 55, 55, 96]);
+
+    let inputData = new Float32Array(product(inputShape.dimensions));
+    randomFill(inputData);
+    let inputDataByteLength = inputData.length * inputData.BYTES_PER_ELEMENT;
+    let input = Module._malloc(inputDataByteLength);
+    let heap = new Uint8Array(Module.HEAPU8.buffer, input, inputDataByteLength);
+    heap.set( new Uint8Array(inputData.buffer) );
+
+    let filterData = new Float32Array(product(filterShape.dimensions));
+    randomFill(filterData);
+    let filterDataByteLength = filterData.length * filterData.BYTES_PER_ELEMENT;
+    let filter = Module._malloc(filterDataByteLength);
+    heap = new Uint8Array(Module.HEAPU8.buffer, filter, filterDataByteLength);
+    heap.set( new Uint8Array(filterData.buffer) );
+
+    let biasData = new Float32Array(product(biasShape.dimensions));
+    randomFill(biasData);
+    let biasDataByteLength = biasData.length * biasData.BYTES_PER_ELEMENT;
+    let bias = Module._malloc(biasDataByteLength);
+    heap = new Uint8Array(Module.HEAPU8.buffer, bias, biasDataByteLength);
+    heap.set( new Uint8Array(biasData.buffer) );
+
+    let output = Module._malloc(product(outputShape.dimensions)*Float32Array.BYTES_PER_ELEMENT);
+
+    let start = performance.now();
+    assert.isTrue(Module.convFloat32(input, inputShape, filter, filterShape, bias, biasShape, padding, padding, padding, padding, stride, stride, Module.NONE, output, outputShape));
+    let end = performance.now();
+    console.log(`convFloat32 elapsed time: ${end - start} ms`);
+
+    Module._free(input);
+    Module._free(filter);
+    Module._free(bias);
+    Module._free(output);
+    inputShape.delete();
+    filterShape.delete();
+    biasShape.delete();
+    outputShape.delete();
   });
 });
